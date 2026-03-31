81ca2618ce9b9b335900902256fcb0b325bc5498
lympe92 <panagiotis.lympe92@gmail.com>
2026-04-01 01:04:47 +0300

    info columns component implemented

--- src/app/shared/components/info-sections/info-columns/info-columns.ts
+++ src/app/shared/components/info-sections/info-columns/info-columns.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
+
+export type InfoColumnsMode = 'default' | 'light';
 
 @Component({
   selector: 'app-info-columns',
@@ -7,4 +9,11 @@ import { Component } from '@angular/core';
   templateUrl: './info-columns.html',
   styleUrl: './info-columns.scss',
 })
-export class InfoColumnsComponent {}
+export class InfoColumnsComponent {
+  mode = input<InfoColumnsMode>('default');
+  items = [
+    { title: '1.000+', description: 'Customers' },
+    { title: '10.000+', description: 'Social followers' },
+    { title: '3 years', description: 'Online' },
+  ];
+}
