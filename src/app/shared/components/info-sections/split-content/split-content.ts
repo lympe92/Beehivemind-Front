23d7d969a5411c20f75991596e74f93bbc265210
lympe92 <panagiotis.lympe92@gmail.com>
2026-04-01 01:37:34 +0300

    split content component

--- src/app/shared/components/info-sections/split-content/split-content.ts
+++ src/app/shared/components/info-sections/split-content/split-content.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ImageComponent} from '../../ui/image/image';
 
 @Component({
   selector: 'app-split-content',
   standalone: true,
-  imports: [],
+  imports: [
+    ImageComponent
+  ],
   templateUrl: './split-content.html',
   styleUrl: './split-content.scss',
 })
