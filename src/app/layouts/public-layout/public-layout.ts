262a8a2f8b79f3ddfd9270c3515b07efe0118584
lympe92 <panagiotis.lympe92@gmail.com>
2026-04-01 02:15:30 +0300

    public header components

--- src/app/layouts/public-layout/public-layout.ts
+++ src/app/layouts/public-layout/public-layout.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {PublicFooterComponent} from '../footer/public-footer/public-footer';
+import { PublicFooterComponent } from '../footer/public-footer/public-footer';
+import { PublicHeaderComponent } from '../header/public-header/public-header';
 
 @Component({
   selector: 'app-public-layout',
   standalone: true,
-  imports: [RouterOutlet, PublicFooterComponent],
+  imports: [RouterOutlet, PublicFooterComponent, PublicHeaderComponent],
   templateUrl: './public-layout.html',
   styleUrl: './public-layout.scss',
 })
