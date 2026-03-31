2342527a54717c43c7f91f9a3fe19a57038552f0
lympe92 <panagiotis.lympe92@gmail.com>
2026-04-01 02:06:21 +0300

    minor components

--- src/app/shared/components/info-sections/text-center-column/text-center-column.ts
+++ src/app/shared/components/info-sections/text-center-column/text-center-column.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {ImageComponent} from '../../ui/image/image';
 
 @Component({
   selector: 'app-text-center-column',
   standalone: true,
-  imports: [],
+  imports: [
+    ImageComponent
+  ],
   templateUrl: './text-center-column.html',
   styleUrl: './text-center-column.scss',
 })
