19ff8196e455ccdbf5cb9d481d9c2e472bd4054e
lympe92 <panagiotis.lympe92@gmail.com>
2026-04-01 02:02:26 +0300

    hero center component

--- src/app/shared/components/hero-sections/hero-center-content/hero-center-content.ts
+++ src/app/shared/components/hero-sections/hero-center-content/hero-center-content.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {ImageComponent} from '../../ui/image/image';
+import {LinkButtonComponent} from '../../ui/link-button/link-button';
 
 @Component({
   selector: 'app-hero-center-content',
   standalone: true,
-  imports: [],
+  imports: [
+    ImageComponent,
+    LinkButtonComponent
+  ],
   templateUrl: './hero-center-content.html',
   styleUrl: './hero-center-content.scss',
 })
