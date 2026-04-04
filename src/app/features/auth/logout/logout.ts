41291d0c438dc2c69ff046f1384514579a01fc6f
lympe92 <panagiotis.lympe92@gmail.com>
2026-04-04 23:42:12 +0300

    auth components implementation

---
+++ src/app/features/auth/logout/logout.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-logout',
+  standalone: true,
+  imports: [],
+  templateUrl: './logout.html',
+  styleUrl: './logout.scss',
+})
+export class LogoutComponent {}
---
+++ src/app/features/auth/request-password/request-password.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-auth-request-password',
+  standalone: true,
+  imports: [],
+  templateUrl: './request-password.html',
+  styleUrl: './request-password.scss',
+})
+export class RequestPasswordComponent {}
