ef74d9714d96ea9429b0748709f3b8cf74f5ead5
lympe92 <panagiotis.lympe92@gmail.com>
2026-04-13 12:37:39 +0300

    google maps api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'https://your-api-domain.com/api',
   appUrl: 'https://beehivemind.tech',
   appName: 'BeehiveMind',
-  googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
-  googleClientId: 'YOUR_GOOGLE_CLIENT_ID',
+  googleMapsApiKey: 'AIzaSyDKZMFseSM5hgOWKr4a8sPFgmN8m5drFLM',
+  googleClientId: '297126212473-v6perq4j86lfl0d6jbem59eu9ujdpqu7.apps.googleusercontent.com',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ export const environment = {
   apiUrl: 'http://127.0.0.1:8000/api/',
   appUrl: 'http://localhost:4200',
   appName: 'BeehiveMind',
-  googleMapsApiKey: 'YOUR_GOOGLE_MAPS_API_KEY',
+  googleMapsApiKey: 'AIzaSyDKZMFseSM5hgOWKr4a8sPFgmN8m5drFLM',
   googleClientId: '297126212473-v6perq4j86lfl0d6jbem59eu9ujdpqu7.apps.googleusercontent.com',
 }
